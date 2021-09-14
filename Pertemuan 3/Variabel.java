14b80dab7ec7a2bf08b97279220a596c90e472e8
AbdilahAzm <abdilahazam.2021@student.uny.ac.id>
2021-09-14 13:35:20 +0700

    mencoba variabel

---
+++ Pertemuan 3/Variabel.java
@@ -0,0 +1,14 @@
+public class Variabel {
+    public static void main(String [] args){
+        String namaDepan = "Abdilah";
+        String namaBelakang = "Azam";
+        int umur = 17;
+        double tinggiBadan = 1.75;
+        double beratBadan = 53.5;
+
+        System.out.println("Nama : " + namaDepan + " " + namaBelakang);
+        System.out.println("Umur : " + umur + " tahun");
+        System.out.println("Tinggi Badan : " + tinggiBadan + " m");
+        System.out.println("Berat Badan : " + beratBadan + " kg");
+    }
+}
