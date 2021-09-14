73448bfc62ddc514072e8fc4bdd8cc06b492ea33
AbdilahAzm <abdilahazam.2021@student.uny.ac.id>
2021-09-14 13:22:05 +0700

    menambahkan comment NIM dan Nama

--- Pertemuan 2/Hello.java
+++ Pertemuan 2/Hello.java
@@ -1,5 +1,10 @@
+/*
+NIM : 21537144026
+Nama : Abdilah Azam
+*/
 public class Hello {
     public static void main(String[] args) {
+        //Untuk menampilkan teks
         System.out.println("Hello, World!");
     }
 }
