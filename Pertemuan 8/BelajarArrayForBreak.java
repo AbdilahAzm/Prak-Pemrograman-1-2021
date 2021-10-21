2c32664123d7e4a8d72fd626981a342749e6071f
AbdilahAzm <abdilahazam.2021@student.uny.ac.id>
2021-10-21 20:55:51 +0700

    belajar array

--- Pertemuan 8/BelajarArrayForBreak.java
+++ Pertemuan 8/BelajarArrayForBreak.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class BelajarArrayForBreak {
     public static void main(String[] args) {
         int[] bilangan = new int[1000];
-        for (int i = 0; i<1000; i++) {
+        for (int i = 0+1; i<1000; i++) {
             Scanner scan = new Scanner(System.in);
-            System.out.print("Masukkan bilangan ke-" + i+1 + " = ");
+            System.out.print("Masukkan bilangan ke-" + i + " = ");
             int cek = scan.nextInt();
             if (cek==0) {
                 break;
@@ -13,11 +13,11 @@ public class BelajarArrayForBreak {
                 bilangan[i] = cek;
             }
         }
-        for (int i = 0; i<bilangan.length; i++) {
+        for (int i = 0+1; i<bilangan.length; i++) {
             if (bilangan[i]==0) {
                 break;
             } else {
-                System.out.println("Bilangan ke-" + i+1 + " = " + bilangan[i]);
+                System.out.println("Bilangan ke-" + i + " = " + bilangan[i]);
             }
         }
     }
